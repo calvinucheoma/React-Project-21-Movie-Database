7df70c2d0653471e3ddaaf020bcf9e71dc90a997
Calvin <calvinchukwuma@gmail.com>
2022-03-29 07:13:36 +0100

    Update SingleMovie.js

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -41,7 +41,7 @@ const SingleMovie = () => {
 
   const {Poster:poster,Title:title,Plot:plot,Year:year} = movie;
   return(
-    <section className="singleMovie">
+    <section className="single-movie">
       <img src={poster} alt={title} />
       <div className="single-movie-info">
         <h2>{title}</h2>
